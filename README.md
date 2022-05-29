af4d5dbd6216ec47104d172b5c58cbd44a0ce61c
Christian Findlay <16697547+MelbourneDeveloper@users.noreply.github.com>
2022-05-29 17:25:32 +1000

    Fix readme

--- README.md
+++ README.md
@@ -5,17 +5,15 @@ Code in `lib/`, and example unit test in `test/`.
 You can do this. It's nice.
 
 ```dart
-
 final a = A('a');
 final builder = IocContainerBuilder();
 builder
-    .addSingleton(a)
-    .add((i) => B(i.get<A>()))
-    .add((i) => C(i.get<B>()))
-    .add((i) => D(i.get<B>(), i.get<C>()));
+  ..addSingletonService(a)
+  ..add((i) => B(i.get<A>()))
+  ..add((i) => C(i.get<B>()))
+  ..add((i) => D(i.get<B>(), i.get<C>()));
 final container = builder.toContainer();
 var d = container.get<D>();
 expect(d.c.b.a, a);
 expect(d.c.b.a.name, 'a');
-
 ```
--- test/ioc_container_test.dart
+++ test/ioc_container_test.dart
@@ -141,6 +141,20 @@ void main() {
         throwsException);
   });
 
+  test('Test Readme', () {
+    final a = A('a');
+    final builder = IocContainerBuilder();
+    builder
+      ..addSingletonService(a)
+      ..add((i) => B(i.get<A>()))
+      ..add((i) => C(i.get<B>()))
+      ..add((i) => D(i.get<B>(), i.get<C>()));
+    final container = builder.toContainer();
+    var d = container.get<D>();
+    expect(d.c.b.a, a);
+    expect(d.c.b.a.name, 'a');
+  });
+
   test('Test Immutability', () {
     final builder = IocContainerBuilder()..addSingletonService(A('a'));
     final container = builder.toContainer();

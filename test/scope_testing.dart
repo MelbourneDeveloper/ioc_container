4842088e6f3013ecca0f53a79243dc9eeb4bdd4a
Christian Findlay <melbournedeveloper@users.noreply.github.com>
2022-10-06 09:23:24 +1100

    Add a scope test

---
+++ test/scope_testing.dart
@@ -0,0 +1,54 @@
+import 'package:ioc_container/ioc_container.dart';
+import 'package:test/test.dart';
+
+class A {
+  A(this.b);
+
+  final B b;
+}
+
+class B {}
+
+class C {
+  C(this.a);
+
+  final A a;
+}
+
+void main() {
+  test('Pudgel', () {
+    final builder = IocContainerBuilder()
+      ..add(
+        (container) => A(
+          container.get<B>(),
+        ),
+      )
+      ..add((container) => B())
+      ..addSingleton<C>(
+        (container) => C(
+          container.get<A>(),
+        ),
+      )
+      ..add<List<Object>>(
+        (container) => [
+          container.get<A>(),
+          container.get<B>(),
+          container.get<C>(),
+        ],
+      );
+    final instance = builder.toContainer(scopeByDefault: true);
+    final scopeObjects = instance.get<List<Object>>();
+    final a = scopeObjects[0] as A;
+    final b = scopeObjects[1] as B;
+    final c = scopeObjects[2] as C;
+
+    expect(
+      identical(a.b, b),
+      true,
+    );
+    expect(
+      identical(c.a, a),
+      true,
+    );
+  });
+}

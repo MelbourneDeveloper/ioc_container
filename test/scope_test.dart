7313da47c66cff98b3ee1f3ab0a732252947bd98
Christian Findlay <melbournedeveloper@users.noreply.github.com>
2022-10-06 09:34:51 +1100

    Narrow down the issue

--- test/scope_test.dart
+++ test/scope_test.dart
@@ -16,39 +16,49 @@ class C {
 }
 
 void main() {
-  test('Test List of Objects', () {
-    final builder = IocContainerBuilder()
-      ..add(
-        (container) => A(
-          container.get<B>(),
-        ),
-      )
-      ..add((container) => B())
-      ..addSingleton<C>(
-        (container) => C(
-          container.get<A>(),
-        ),
-      )
-      ..add<List<Object>>(
-        (container) => [
-          container.get<A>(),
-          container.get<B>(),
-          container.get<C>(),
-        ],
-      );
-    final instance = builder.toContainer();
-    final scopeObjects = instance.getScoped<List<Object>>();
-    final a = scopeObjects[0] as A;
-    final b = scopeObjects[1] as B;
-    final c = scopeObjects[2] as C;
-
-    expect(
-      identical(a.b, b),
-      true,
-    );
-    expect(
-      identical(c.a, a),
-      true,
-    );
+  test('Test List of Objects Lazy', () {
+    testScoped(true);
   });
+
+  test('Test List of Objects Zealous', () {
+    testScoped(false);
+  });
+}
+
+void testScoped(bool isLazy) {
+  final builder = IocContainerBuilder()
+    ..add(
+      (container) => A(
+        container.get<B>(),
+      ),
+    )
+    ..add((container) => B())
+    ..addSingleton<C>(
+      (container) => C(
+        container.get<A>(),
+      ),
+    )
+    ..add<List<Object>>(
+      (container) => [
+        container.get<A>(),
+        container.get<B>(),
+        container.get<C>(),
+      ],
+    );
+
+  final instance = builder.toContainer(isLazy: isLazy);
+  final scoped = instance.scoped();
+  final scopeObjects = scoped.get<List<Object>>();
+  final a = scopeObjects[0] as A;
+  final b = scopeObjects[1] as B;
+  final c = scopeObjects[2] as C;
+
+  expect(
+    identical(a.b, b),
+    true,
+  );
+  expect(
+    identical(c.a, a),
+    true,
+  );
 }

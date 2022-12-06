173d5d5f3031a7652c012adef677e6fb06f9d867
Christian Findlay <melbournedeveloper@users.noreply.github.com>
2022-12-06 14:24:25 +1100

    Move the files

--- lib/example.dart
+++ lib/example.dart
@@ -1,7 +1,10 @@
+library example;
+
 // ignore_for_file: public_member_api_docs
 
 //flutter pub run build_runner build lib --delete-conflicting-outputs
 
 part 'example.ioc.dart';
+
 
 class Example {}

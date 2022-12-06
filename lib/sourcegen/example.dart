3bc8237486aa4c733b12c7e26a90c8bba1fa87f9
Christian Findlay <melbournedeveloper@users.noreply.github.com>
2022-12-06 13:52:41 +1100

    Comments

--- lib/sourcegen/example.dart
+++ lib/sourcegen/example.dart
@@ -1,8 +1,7 @@
 // ignore_for_file: public_member_api_docs
 
+//flutter pub run build_runner build lib --delete-conflicting-outputs
+
 part 'example.ioc.dart';
 
-class Example
-{
-
-}
+class Example {}

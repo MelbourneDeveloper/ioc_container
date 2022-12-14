4b6f23ebe788910274fcadfd2309a4ee20d37a92
Christian Findlay <melbournedeveloper@users.noreply.github.com>
2022-12-15 08:26:15 +1100

    Get this branch working again

--- lib/example.dart
+++ lib/example.dart
@@ -1,11 +1,10 @@
 // ignore_for_file: public_member_api_docs, avoid_unused_constructor_parameters
 
+// flutter pub run build_runner build --delete-conflicting-outputs
+
 library example;
 
 import 'package:ioc_container/ioc_container.dart';
-
-@FactoryDefinition(isSingleton: false)
-Example newExample(IocContainer container) => const Example();
 
 class FactoryDefinition {
   const FactoryDefinition({required this.isSingleton});
@@ -13,14 +12,9 @@ class FactoryDefinition {
   final bool isSingleton;
 }
 
-//@ServiceDefinition(newExample, isSingleton: true)
 class Example {
   const Example();
 
-  //@FactoryDefinition(isSingleton: false)
+  @FactoryDefinition(isSingleton: false)
   factory Example.fromContainer(IocContainer container) => const Example();
-
-  // @FactoryDefinition(isSingleton: false)
-  // static Future<Example> fromContainerAsync(IocContainer container) async =>
-  //     const Example();
 }

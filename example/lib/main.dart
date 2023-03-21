0a0ea3fb6e4423fc5d1318e5103d96efad07da0d
Christian Findlay <16697547+MelbourneDeveloper@users.noreply.github.com>
2023-03-22 09:09:48 +1100

    New example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,128 +1,89 @@
 import 'package:flutter/material.dart';
-import 'package:retry/retry.dart';
 import 'package:ioc_container/ioc_container.dart';
 
-//Note this example throws exceptions on purpose. See the notes below.
+///A service that the theme controller depends on
+class SettingsService {
+  bool get isLightMode => true;
+}
 
-///This is the business logic for our app and accepts a [FlakyService]
-class AppChangeNotifier extends ChangeNotifier {
-  AppChangeNotifier(this._flakyService);
+///A controller for the theme
+class ThemeNotifier extends ChangeNotifier {
+  ThemeNotifier(this.settingsService) {
+    _themeData = _getTheme(settingsService.isLightMode);
+  }
 
-  final FlakyService _flakyService;
-  int counter = 0;
+  final SettingsService settingsService;
+  late ThemeData _themeData;
 
-  void increment() {
-    counter++;
+  ThemeData get themeData => _themeData;
+
+  void toggleTheme() {
+    _themeData = _getTheme(_themeData.brightness == Brightness.dark);
     notifyListeners();
   }
+
+  ThemeData _getTheme(bool isLightMode) =>
+      ThemeData(primarySwatch: Colors.purple).copyWith(
+        brightness: isLightMode ? Brightness.light : Brightness.dark,
+      );
 }
 
-class FlakyService {
-  static int retryCount = 0;
-}
+///Configure the services
+final IocContainerBuilder builder = IocContainerBuilder()
+  ..addSingletonService(SettingsService())
+  ..addSingleton(
+    (container) => ThemeNotifier(container<SettingsService>()),
+  );
 
-///This is the composition root. This is where we compose or "wire up" our dependencies.
-IocContainerBuilder compose({bool allowOverrides = false}) =>
-    IocContainerBuilder(allowOverrides: allowOverrides)
-      ..addSingletonAsync<AppChangeNotifier>(
-        (container) => Future.delayed(
-          const Duration(milliseconds: 50),
-          () async => AppChangeNotifier(
-            await container.getAsync<FlakyService>(),
-          ),
-        ),
-      )
-      ..addSingletonAsync((container) async {
-        if (FlakyService.retryCount < 5) {
-          FlakyService.retryCount++;
-          debugPrint(
-              'FlakyService failed to initialize ${FlakyService.retryCount}x. Retrying...');
-          //This service fails to initialize the first time around
-          throw Exception();
-        }
-
-        return FlakyService();
-      });
+late final IocContainer serviceLocator;
 
 void main() {
-  runApp(
-    MyApp(
-      //We pass the container in to the root widget
-      container: compose().toContainer(),
-    ),
-  );
+  //Build the container/service locator
+  serviceLocator = builder.toContainer();
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({
-    required this.container,
-    super.key,
-  });
-
-  final IocContainer container;
+  const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'ioc_container Example',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      //We use a FutureBuilder to wait for the initialization to complete
-      home: FutureBuilder(
-        //Add resiliency by retrying the initialization of the FlakyService until it succeeds
-        future: retry(
-            delayFactor: const Duration(milliseconds: 50),
-            //getAsyncSafe ensures we don't stored the failed initialization in the container
-            () async => container.getAsyncSafe<AppChangeNotifier>()),
-        builder: (c, s) => s.data == null
-            //We display a progress indicator until the Future completes
-            ? const CircularProgressIndicator.adaptive()
-            : AnimatedBuilder(
-                animation: s.data!,
-                builder: (context, bloobit) => MyHomePage(
-                  title: 'ioc_container Example',
-                  appChangeNotifier: s.data!,
-                ),
+  Widget build(BuildContext context) => AnimatedBuilder(
+        //Listen to changes in the theme and rebuild when they occur
+        animation: serviceLocator<ThemeNotifier>(),
+        builder: (context, child) => MaterialApp(
+          //Use the theme in the controller
+          theme: serviceLocator<ThemeNotifier>().themeData,
+          debugShowCheckedModeBanner: false,
+          title: 'Theme Switcher',
+          home: Scaffold(
+            appBar: AppBar(title: const Text('Theme Switcher')),
+            body:
+                const Center(child: Text('Press the button to toggle theme.')),
+            floatingActionButton: Builder(
+              builder: (context) => FloatingActionButton(
+                onPressed: () {
+                  //Toggle the theme and show a snackbar
+                  final themeNotifier = serviceLocator<ThemeNotifier>()
+                    ..toggleTheme();
+                  final brightness =
+                      themeNotifier.themeData.brightness == Brightness.dark
+                          ? 'dark'
+                          : 'light';
+                  ScaffoldMessenger.of(context).removeCurrentSnackBar();
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      elevation: 1,
+                      content: Text(
+                        'Theme toggled to $brightness',
+                        style: const TextStyle(color: Colors.purple),
+                      ),
+                    ),
+                  );
+                },
+                child: const Icon(Icons.lightbulb_outline),
               ),
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({
-    required this.appChangeNotifier,
-    required this.title,
-    super.key,
-  });
-
-  final String title;
-  final AppChangeNotifier appChangeNotifier;
-
-  @override
-  Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
-          title: Text(title),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text(
-                'You have pushed the button this many times:',
-              ),
-              Text(
-                '${appChangeNotifier.counter}',
-                style: Theme.of(context).textTheme.headline4,
-              ),
-            ],
+            ),
           ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: appChangeNotifier.increment,
-          tooltip: 'Increment',
-          child: const Icon(Icons.add),
         ),
       );
 }
